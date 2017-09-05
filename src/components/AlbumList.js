b86fc819a401c2908e08749b096741e8e8c7f2b9
Daniel Velez <velez.a.daniel@gmail.com>
2017-09-04 22:27:41 -0400

    first commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+
+  componentWillMount() {
+    console.log('componentWillMount in album list');
+  }
+
+  render() {
+      return (
+        <View>
+        <Text>Album list!!</Text>
+        </View>
+    );
+  }
+}
+
+
+export default AlbumList;
